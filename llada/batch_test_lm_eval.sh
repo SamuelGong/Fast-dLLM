d5d7e39d42c0bb1295ad1a69e5c1348a44b0c448
Zhifeng Jiang <samuelgong2017@gmail.com>
2025-08-02 17:46:46 +0800

    Minor.

--- llada/batch_test_lm_eval.sh
+++ llada/batch_test_lm_eval.sh
@@ -54,7 +54,7 @@ while (( bl <= length )); do
     out_dir="${output_root}/${task}/${method}/${length}/${bl}/${st}"
     mkdir -p "${out_dir}"
 
-    accelerate launch "${script}" --tasks "${task}" \
+    accelerate launch --num_processes 4 "${script}" --tasks "${task}" \
       --confirm_run_unsafe_code --model "${model}" \
       --model_args "model_path=${model_path},gen_length=${length},steps=${st},block_length=${bl},use_kv_cache=${method},show_speed=True" \
       --output_path "${out_dir}" --log_samples
