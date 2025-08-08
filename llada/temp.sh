cda54802fd1905f610b5977d3fea958348873618
Zhifeng Jiang <samuelgong2017@gmail.com>
2025-08-08 11:06:22 +0800

    A

--- llada/temp.sh
+++ llada/temp.sh
@@ -1,5 +1,5 @@
-bash utility_profile.sh C2F --num_processes 4 --main_process_port 29500 --task humaneval --length 512
+bash utility_profile.sh C2F --num_processes 4 --main_process_port 29500 --task gsm8k --length 128
 sleep 5
-bash utility_profile.sh Dual --num_processes 4 --main_process_port 29500 --task humaneval --length 512
+bash utility_profile.sh Dual --num_processes 4 --main_process_port 29500 --task gsm8k --length 128
 sleep 5
-bash utility_profile.sh None --num_processes 4 --main_process_port 29500 --task humaneval --length 512
+bash utility_profile.sh None --num_processes 4 --main_process_port 29500 --task gsm8k --length 128
