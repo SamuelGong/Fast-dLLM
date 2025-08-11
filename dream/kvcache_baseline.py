ab80cb013ec99d255ff016388388081c6cacb9ec
Zhifeng Jiang <samuelgong2017@gmail.com>
2025-08-11 15:44:36 +0800

    A

--- dream/kvcache_baseline.py
+++ dream/kvcache_baseline.py
@@ -12,12 +12,14 @@ import argparse
 from contextlib import contextmanager
 import torch
 from transformers import AutoTokenizer, AutoModel
-
-from generate import (generate, generate_with_prefix_cache,
-                      generate_with_dual_cache, generate_with_finegrained_cache,
-                      generate_with_dual_cache_and_q_cache, generate_coarse_to_fine)
 from torch.profiler import profile, ProfilerActivity
 from model.modeling_dream import DreamModel
+from model.generation_utils_block import DreamGenerationMixin
+import types
+
+import sys
+sys.path.append('../')
+from llada.kvcache_baseline import evaluate_qa
 
 # ───────────────────────────────────────── config
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -38,46 +40,70 @@ def cuda_timer(label="Elapsed"):
 # ─────────────────────────────────── benchmark helper
 
 
-def benchmark(prompt, tokenizer, *, steps, gen_len, block_len, use_kv_cache):
+def benchmark(inputs, tokenizer, *, steps, gen_len, block_len, use_kv_cache, debug=False):
     tag = use_kv_cache
     print(f"\nLoading model for {tag} …")
     # model = AutoModel.from_pretrained(MODEL_NAME, trust_remote_code=True, torch_dtype=DTYPE).to(DEVICE).eval()
     model = DreamModel.from_pretrained(MODEL_NAME, trust_remote_code=True, torch_dtype=DTYPE).to(DEVICE).eval()
+    model.diffusion_generate = types.MethodType(DreamGenerationMixin.diffusion_generate, model)
+    model._sample = types.MethodType(DreamGenerationMixin._sample, model)
 
     # warm‑up
     with torch.inference_mode():
-        _ = model(prompt[:, :1]); torch.cuda.synchronize()
+        _ = model(
+            inputs.input_ids[, :1],
+            attention_mask=inputs.attention_mask[, :1]
+        ); torch.cuda.synchronize()
 
     # seq_len = prompt.shape[1] + gen_len
     # attach_qcache_monkey(model, prompt.shape[1] + gen_len) if use_qcache else None
     with cuda_timer(f"{tag}") as get_elapsed:
         with profile(activities=[ProfilerActivity.CUDA]) as prof:
             if use_kv_cache == "None":
-                out, nfe = generate(model, prompt, steps=steps, gen_length=gen_len,
-                               block_length=block_len, temperature=0.,
-                               remasking='low_confidence', tokenizer=tokenizer)
+                # out, nfe = generate(model, prompt, steps=steps, gen_length=gen_len,
+                #                block_length=block_len, temperature=0.,
+                #                remasking='low_confidence', tokenizer=tokenizer)
+                output = model.diffusion_generate(
+                    inputs.input_ids,
+                    attention_mask=inputs.attention_mask,
+                    max_new_tokens=gen_len,
+                    output_history=True,
+                    return_dict_in_generate=True,
+                    steps=steps,
+                    temperature=0.,
+                    block_length=block_len,
+                    # generation_tokens_hook_func=generation_tokens_hook_func
+                )
             elif use_kv_cache == "Prefix":
-                out, nfe = generate_with_prefix_cache(model, prompt, steps=steps, gen_length=gen_len,
-                               block_length=block_len, temperature=0.,
-                               remasking='low_confidence', tokenizer=tokenizer)
+                pass
+                # out, nfe = generate_with_prefix_cache(model, prompt, steps=steps, gen_length=gen_len,
+                #                block_length=block_len, temperature=0.,
+                #                remasking='low_confidence', tokenizer=tokenizer)
             elif use_kv_cache == "Dual":
-                out, nfe = generate_with_dual_cache(model, prompt, steps=steps, gen_length=gen_len,
-                               block_length=block_len, temperature=0.,
-                               remasking='low_confidence', tokenizer=tokenizer)
+                pass
+                # out, nfe = generate_with_dual_cache(model, prompt, steps=steps, gen_length=gen_len,
+                #                block_length=block_len, temperature=0.,
+                #                remasking='low_confidence', tokenizer=tokenizer)
             elif use_kv_cache == "C2F":
-                out, nfe = generate_coarse_to_fine(model, prompt, steps=steps, gen_length=gen_len,
-                                                   block_length=block_len, temperature=0.,
-                                                   remasking='low_confidence', tokenizer=tokenizer)
+                pass
+                # out, nfe = generate_coarse_to_fine(model, prompt, steps=steps, gen_length=gen_len,
+                #                                    block_length=block_len, temperature=0.,
+                #                                    remasking='low_confidence', tokenizer=tokenizer, debug=debug)
+
     # decode and show (outside timing)
-    answer = tokenizer.batch_decode(out[:, prompt.shape[1]:], skip_special_tokens=True)[0]
+    elapsed_seconds = get_elapsed()
+    answer = tokenizer.decode(output.sequences[0][len(inputs.input_ids[0]):].tolist())
+    answer = answer.split(tokenizer.eos_token)[0].strip()
     print(f"{tag} output → {answer}\n")
     # print(prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=40))
 
     # print(prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=20))
-    # print(prof.key_averages().table(row_limit=20))
+    # print(prof.ke
+    # _averages().table(row_limit=20))
 
     # free memory
     del model; torch.cuda.empty_cache()
+    return elapsed_seconds, answer
 
 # ─────────────────────────────────────── main
 
@@ -90,18 +116,19 @@ def main():
     # ap.add_argument("--steps", type=int, default=512)
     # ap.add_argument("--gen", type=int, default=512)
     ap.add_argument("--block", type=int, default=4)
+    ap.add_argument("--debug", action="store_true")
     args = ap.parse_args()
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
-    prompt_txt = tokenizer.apply_chat_template([{"role": "user", "content": args.question}], add_generation_prompt=True, tokenize=False)
-    prompt = torch.tensor(tokenizer(prompt_txt)["input_ids"], device=DEVICE).unsqueeze(0)
+    inputs = tokenizer.apply_chat_template([{"role": "user", "content": args.question}], add_generation_prompt=True)
 
-    # benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="None")
-    # benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="Prefix")
-    benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="Dual")
-    # benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="Fine")
-    # benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="DualAndQuery")
-    # benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="C2F")
+    # lat, answer = benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="None")
+    # lat, answer = benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="Prefix")
+    lat, answer = benchmark(inputs, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block, use_kv_cache="Dual")
+    # lat, answer = benchmark(prompt, tokenizer, steps=args.steps, gen_len=args.gen, block_len=args.block,
+    #                         use_kv_cache="C2F", debug=args.debug)
+    evaluation = evaluate_qa(args.question, answer)
+    print(lat, evaluation)
 
 
 if __name__ == "__main__":
