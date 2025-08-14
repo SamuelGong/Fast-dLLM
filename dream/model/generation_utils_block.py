83ec8ecbb1af4e61b86e61a2cebfff7d1f25f6e5
Zhifeng Jiang <samuelgong2017@gmail.com>
2025-08-14 13:35:02 +0800

    A

--- dream/model/generation_utils_block.py
+++ dream/model/generation_utils_block.py
@@ -493,9 +493,8 @@ class DreamGenerationMixin:
                 block_sel = torch.zeros_like(x0, dtype=torch.bool, device=x0.device)
                 for j in range(confidence.shape[0]):
                     _, select_index = torch.topk(confidence[j], k=block_length)
-                    print(select_index)
-                    select_index += 4
-                    print(select_index)
+                    select_index = select_index + 4
+                    select_index = torch.max(select_index, 152)
 
                     # TODO: It seems that I have to use this logic though I do not know why
                     # Check if first_idx is already in select_index
